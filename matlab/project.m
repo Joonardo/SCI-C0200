5b2c13c8a94c998a01c7c901cbf409236806345e
Joonardo <joonardob@gmail.com>
2016-05-05 11:38:30 +0300

    fixed problem with line length.

--- matlab/project.m
+++ matlab/project.m
@@ -8,8 +8,8 @@ function [] = project()
     while t < 8*60
         arriving = realisation(lambda, t, t + 1);
         leaving = realisation(1/mu_j(mu, s, j(end)), t, t + 1);
-        j(end + 1) = j(end) + arriving - leaving;
-        t = t + 1
+        j(end + 1) = max(j(end) + arriving - leaving, 0);
+        t = t + 1;
+        stairs(0:t, j)
     end
-    stairs(0:8*60, j)
 end
